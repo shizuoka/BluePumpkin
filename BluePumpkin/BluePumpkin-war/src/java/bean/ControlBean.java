04cfa78e1d485019dfc833bd94f63fb8220a5dd8
bui trung thanh <thanhbt_C00411@fpt.aptech.ac.vn>
2013-05-21 23:43:40 +0700

    thanh add controlBean

---
+++ BluePumpkin/BluePumpkin-war/src/java/bean/ControlBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author TrungThanh
+ */
+@ManagedBean
+@SessionScoped
+public class ControlBean {
+
+    /**
+     * Creates a new instance of ControlBean
+     */
+    public ControlBean() {
+    }
+}
